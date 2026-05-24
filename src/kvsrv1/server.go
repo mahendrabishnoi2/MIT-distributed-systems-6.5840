bde26b1b0612bf885223b35c757977cf79ce519e
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2026-05-24 08:34:06 +0530

    lab 2 kv server: kv server with reliable network

--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -18,23 +18,39 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type kvStoreValue struct {
+	value   string
+	version rpc.Tversion
+}
 
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	kvStore map[string]kvStoreValue
 }
 
 func MakeKVServer() *KVServer {
-	kv := &KVServer{}
-	// Your code here.
+	kv := &KVServer{
+		kvStore: make(map[string]kvStoreValue),
+	}
 	return kv
 }
 
 // Get returns the value and version for args.Key, if args.Key
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	val, ok := kv.kvStore[args.Key]
+	if !ok {
+		reply.Err = rpc.ErrNoKey
+		return
+	}
+	reply.Value = val.value
+	reply.Version = val.version
+	reply.Err = rpc.OK
+	return
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -42,13 +58,31 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // If the key doesn't exist, Put installs the value if the
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	val, ok := kv.kvStore[args.Key]
+	if ok {
+		if val.version != args.Version {
+			reply.Err = rpc.ErrVersion
+			return
+		}
+	}
+	if !ok && args.Version != 0 {
+		reply.Err = rpc.ErrNoKey
+		return
+	}
+
+	kv.kvStore[args.Key] = kvStoreValue{
+		value:   args.Value,
+		version: args.Version + 1,
+	}
+	reply.Err = rpc.OK
+	return
 }
 
 // You can ignore Kill() for this lab
 func (kv *KVServer) Kill() {
 }
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []tester.IService {
